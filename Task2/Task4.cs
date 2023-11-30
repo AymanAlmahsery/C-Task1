c2155ce510e5075fb451e3359f855f28e733047b
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 23:36:00 +0300

    Create Task4.cs

---
+++ Task2/Task4.cs
@@ -0,0 +1,39 @@
+﻿int num1, num2, num3, num4, num5;
+
+Console.Write("Enter first number: ");
+int.TryParse(Console.ReadLine(), out num1);
+
+Console.Write("Enter second number: ");
+int.TryParse(Console.ReadLine(), out num2);
+
+Console.Write("Enter third number: ");
+int.TryParse(Console.ReadLine(), out num3);
+
+Console.Write("Enter fourth number: ");
+int.TryParse(Console.ReadLine(), out num4);
+
+Console.Write("Enter fifth number: ");
+int.TryParse(Console.ReadLine(), out num5);
+
+int maxNu = num1;
+
+if (num2 > maxNu)
+{
+    maxNu = num2;
+}
+
+if (num3 > maxNu)
+{
+    maxNu = num3;
+}
+
+if (num4 > maxNu)
+{
+    maxNu = num4;
+}
+
+if (num5 > maxNu)
+{
+    maxNu = num5;
+}
+Console.WriteLine($"The maximum number is: {maxNu}");
