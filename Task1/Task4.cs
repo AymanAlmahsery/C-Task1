6bb865741e3ab16f80849c156a4cbc62d99749dd
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 23:35:48 +0300

    Create Task4.cs

---
+++ Task1/Task4.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information 
+Console.WriteLine("Input your firstname : ");
+string fname = Console.ReadLine();
+Console.WriteLine("Input your lastname : ");
+string Sname = Console.ReadLine();
+Console.WriteLine("Input year of birth : ");
+int year = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine(fname +"  "+ Sname + "  " + year);
