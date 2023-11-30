8145acccb5a880305843892d9980e7a038fa17fc
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 23:35:54 +0300

    Create Task2.cs

---
+++ Task2/Task2.cs
@@ -0,0 +1,15 @@
+﻿int number;
+Console.Write("Enter a number: ");
+int.TryParse(Console.ReadLine(), out number);
+if (number > 0)
+{
+    Console.WriteLine("The sign is +");
+}
+else if (number < 0)
+{
+    Console.WriteLine("The sign is -");
+}
+else
+{
+    Console.WriteLine("The number is zero");
+}
