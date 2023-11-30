cca9e6f176d8fcb5a2d92c52146f606cc82b24ac
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 23:36:03 +0300

    Create Task5.cs

---
+++ Task2/Task5.cs
@@ -0,0 +1,7 @@
+﻿double km, miles;
+Console.Write("Input kilometers per hour: ");
+double.TryParse(Console.ReadLine(), out km);
+
+miles = km * 0.621371;
+
+Console.WriteLine($"{km} kilometers per hour is equal to {miles} miles per hour.");
