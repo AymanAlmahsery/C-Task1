f30c2e4b8ec2a47c29676abd16b0c24a047444ea
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 23:36:11 +0300

    Create Task7.cs

---
+++ Task2/Task7.cs
@@ -0,0 +1,9 @@
+﻿int totalMinutes;
+
+Console.Write("Input minutes: ");
+int.TryParse(Console.ReadLine(), out totalMinutes);
+
+int hours = totalMinutes / 60;
+int remainingMinutes = totalMinutes % 60;
+
+Console.WriteLine($"{hours} Hours, {remainingMinutes} Minutes");
