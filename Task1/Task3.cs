546a14d49fe7b08a868fbdbb47eede5ba968b49a
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 22:56:41 +0300

    Create Task3.cs

---
+++ Task1/Task3.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] b = new string[] { "BMW", "Honda", "Mercedes" };
+foreach (string car in b)
+{
+    Console.WriteLine(car);
+}
