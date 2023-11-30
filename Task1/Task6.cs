85b692cfa8515d351c152b83ccfc268344192faa
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 23:35:51 +0300

    Create Task6.cs

---
+++ Task1/Task6.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[3];
+Console.WriteLine("Enter elements into the array:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"index - {i} : ");
+    int.TryParse(Console.ReadLine(), out array[i]);
+}
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    sum += array[i];
+}
+Console.WriteLine($"Sum of all elements stored in the array is: {sum}");
