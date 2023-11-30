5574fbc07633e37f73162ec3e73d80a81029e619
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 23:35:43 +0300

    Create Task2.cs

---
+++ Task1/Task2.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a = 5.5;
+        string b = "Ayman";
+        char c = 'a';
+        bool d = true;
+        int i = 0;
+        const int j = 1;
+
+    }
+}
