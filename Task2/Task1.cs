de33ab92d5c66734214b45977570df298c2532f1
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 22:56:47 +0300

    Create Task1.cs

---
+++ Task2/Task1.cs
@@ -0,0 +1,17 @@
+﻿int num1, num2;
+Console.Write("Enter the first integer: ");
+int.TryParse(Console.ReadLine(), out num1);
+Console.Write("Enter the second integer: ");
+int.TryParse(Console.ReadLine(), out num2);
+int smallNu;
+if (num1 < num2)
+{
+    smallNu = num1;
+}
+else
+{
+    smallNu = num2;
+}
+
+// Display the smaller number
+Console.WriteLine($"The smaller number is: {smallNu}");
