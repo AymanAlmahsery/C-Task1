6ac1a72bd11883e58436a02ce5fcc2d26c059d10
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 22:56:44 +0300

    Create Task5.cs

---
+++ Task1/Task5.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[10];
+
+Console.WriteLine("Input 10 elements in the array:");
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"element - {i} : ");
+    int.TryParse(Console.ReadLine(), out array[i]);
+}
+Console.Write("Elements in array are: ");
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
