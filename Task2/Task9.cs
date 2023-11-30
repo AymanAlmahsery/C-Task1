0c4745622493bde399d23ccb36608522b1f31534
Ayman Ali <115194309+AymanAlmahsery@users.noreply.github.com>
2023-11-30 23:36:19 +0300

    Create Task9.cs

---
+++ Task2/Task9.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(reverseOdd("Bananas")); // Output: sananaB
+Console.WriteLine(reverseOdd("One two three four")); // Output: enO owt eerht four
+
+static string reverseOdd(string input)
+{
+    string[] words = input.Split(' ');
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length % 2 != 0)
+        {
+            words[i] = string.Concat(words[i].Reverse());
+        }
+    }
+
+    return string.Join(" ", words);
+}
